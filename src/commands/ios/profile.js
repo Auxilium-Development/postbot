9f3c57a439e6834d0202efadaa1b572f8b29badc
Jeroen Claassens <jeroen.claassens@live.nl>
2018-06-04 00:28:23 +0200

    Spelling

--- src/commands/ios/profile.js
+++ src/commands/ios/profile.js
@@ -9,7 +9,7 @@ module.exports = class ProfileCommand extends Command {
       memberName: 'profile',
       group: 'ios',
       alias: ['prof'],
-      description: 'Get a link to the TVOS profile for blocking updates',
+      description: 'Get a link to the tvOS profile for blocking updates',
       guildOnly: false,
       throttling: {
         usages: 2,
@@ -21,7 +21,7 @@ module.exports = class ProfileCommand extends Command {
   run (msg) {
     deleteCommandMessages(msg, this.client);
 
-    return msg.reply(stripIndents`To prevent OTA iOS updated download the TvOS beta profile by opening this link in mobile Discord or Safari
+    return msg.reply(stripIndents`To prevent OTA iOS updated download the tvOS beta profile by opening this link in mobile Discord or Safari
     https://cdn.discordapp.com/attachments/355816702002331658/412689441547878401/tvOS11_1.mobileconfig`);
   }
 };
